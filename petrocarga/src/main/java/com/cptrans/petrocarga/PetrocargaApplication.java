f04b166b11c81a0636f9bd3b8cc92dd1e7a7491f
IgorAPSantos <igorppandre@hotmail.com>
2025-10-01 00:27:45 -0300

    criação projeto java

---
+++ petrocarga/src/main/java/com/cptrans/petrocarga/PetrocargaApplication.java
@@ -0,0 +1,10 @@
+package com.cptrans.petrocarga;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrocargaApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(PetrocargaApplication.class, args);
+    }
+}
---
+++ petrocarga/src/test/java/com/cptrans/petrocarga/petrocarga/PetrocargaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cptrans.petrocarga.petrocarga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrocargaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
