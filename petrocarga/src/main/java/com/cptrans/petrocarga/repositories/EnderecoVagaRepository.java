619a5f6726a9f58367deb0561bb6dbe69d7efb98
IgorAPSantos <igorppandre@hotmail.com>
2025-10-01 00:32:16 -0300

    add EnderecoVagaRepository

---
+++ petrocarga/src/main/java/com/cptrans/petrocarga/repositories/EnderecoVagaRepository.java
@@ -0,0 +1,14 @@
+package com.cptrans.petrocarga.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cptrans.petrocarga.models.EnderecoVaga;
+
+@Repository
+public interface EnderecoVagaRepository extends JpaRepository<EnderecoVaga, Integer>{
+    public Optional<EnderecoVaga> findByCodigoPmp(String codigoPmp);
+    public Optional<EnderecoVaga> findByRua(String rua);
+}
