1362e9ade116a9fd391e78d45f6ddde43622e1c9
IgorAPSantos <igorppandre@hotmail.com>
2025-10-02 20:41:12 -0300

    feat: cadastrarEnderecoVaga

--- petrocarga/src/main/java/com/cptrans/petrocarga/services/EnderecoVagaService.java
+++ petrocarga/src/main/java/com/cptrans/petrocarga/services/EnderecoVagaService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cptrans.petrocarga.dto.EnderecoVagaRequestDTO;
 import com.cptrans.petrocarga.models.EnderecoVaga;
 import com.cptrans.petrocarga.repositories.EnderecoVagaRepository;
 
@@ -13,6 +14,37 @@ public class EnderecoVagaService {
     
     @Autowired
     private EnderecoVagaRepository enderecoVagaRepository;
+
+    public EnderecoVaga cadastrarEnderecoVaga(EnderecoVagaRequestDTO enderecoVagaRequest){
+        try {
+            if(enderecoVagaRequest == null) {
+                throw new IllegalArgumentException("O objeto EnderecoVagaRequestDTO não pode ser nulo.");
+            }
+            EnderecoVaga enderecoVagaCadastrado = findEnderecoByCodigo_pmp(enderecoVagaRequest.getCodigoPMP());
+            if(enderecoVagaCadastrado != null) {
+                return enderecoVagaCadastrado;
+            }
+            if(enderecoVagaRequest.getCodigoPMP() == null || enderecoVagaRequest.getCodigoPMP().isEmpty()) {
+                throw new IllegalArgumentException("O campo 'codigoPMP' é obrigatório e não pode ser nulo ou vazio.");
+            }
+            if(enderecoVagaRequest.getLogradouro() == null || enderecoVagaRequest.getLogradouro().isEmpty()) {
+                throw new IllegalArgumentException("O campo 'logradouro' é obrigatório e não pode ser nulo ou vazio.");
+            }
+            if(enderecoVagaRequest.getBairro() == null || enderecoVagaRequest.getBairro().isEmpty()) {
+                throw new IllegalArgumentException("O campo 'bairro' é obrigatório e não pode ser nulo ou vazio.");
+            }
+            if(enderecoVagaRepository.findByCodigoPmp(enderecoVagaRequest.getCodigoPMP()).isPresent()) {
+                throw new IllegalArgumentException("Já existe um endereço cadastrado com o código PMP: " + enderecoVagaRequest.getCodigoPMP());
+            }
+            EnderecoVaga enderecoVaga = new EnderecoVaga();
+            enderecoVaga.setCodigoPmp(enderecoVagaRequest.getCodigoPMP());
+            enderecoVaga.setLogradouro(enderecoVagaRequest.getLogradouro());
+            enderecoVaga.setBairro(enderecoVagaRequest.getBairro());
+            return enderecoVagaRepository.save(enderecoVaga);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Erro ao cadastrar endereço: " + e.getMessage());
+        }
+    }
 
     public EnderecoVaga findEnderecoByCodigo_pmp(String codigoPmp){
         try {
