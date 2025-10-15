16afa76d2639f1472807ba7f4cd200c2d1029301
IgorAPSantos <igorppandre@hotmail.com>
2025-10-15 16:23:40 -0300

    add execption e retira @size do vagaRequest

--- petrocarga/src/main/java/com/cptrans/petrocarga/exceptions/GlobalHandlerException.java
+++ petrocarga/src/main/java/com/cptrans/petrocarga/exceptions/GlobalHandlerException.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -58,5 +59,13 @@ public class GlobalHandlerException {
         error.put("cause", causeMessage);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getAllErrors().getFirst().getDefaultMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
 
 }
