5b72e4db66a0db69e2d37f6487fdf68ff5fe0689
IgorAPSantos <igorppandre@hotmail.com>
2025-10-15 23:12:38 -0300

    fix: tentativa de liberar CORS

--- petrocarga/src/main/java/com/cptrans/petrocarga/configs/WebConfig.java
+++ petrocarga/src/main/java/com/cptrans/petrocarga/configs/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET","POST","PUT","DELETE");
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
