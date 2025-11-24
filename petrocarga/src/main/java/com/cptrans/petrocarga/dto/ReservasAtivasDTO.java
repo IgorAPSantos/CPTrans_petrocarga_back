dcff314ef8c8043a9902c17ae7d8db04e8b2ef83
IgorAPSantos <igorppandre@hotmail.com>
2025-11-24 09:37:04 -0300

    fix refactor:  get reservas ativas

---
+++ petrocarga/src/main/java/com/cptrans/petrocarga/dto/ReservasAtivasDTO.java
@@ -0,0 +1,55 @@
+package com.cptrans.petrocarga.dto;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import com.cptrans.petrocarga.models.Vaga;
+
+
+public class ReservasAtivasDTO {
+    private UUID vagaId;
+    private OffsetDateTime inicio;
+    private OffsetDateTime fim;
+    private Integer tamanhoVeiculo;
+    private String placaVeiculo;
+
+    public ReservasAtivasDTO() {
+    }
+    public ReservasAtivasDTO(Vaga vaga,OffsetDateTime inicio, OffsetDateTime fim, Integer tamanhoVeiculo, String placaVeiculo) {
+        this.vagaId = vaga.getId();
+        this.inicio = inicio;
+        this.fim = fim;
+        this.tamanhoVeiculo = tamanhoVeiculo;
+        this.placaVeiculo = placaVeiculo;
+    }
+    public UUID getVaga() {
+        return vagaId;
+    }
+    public void setVaga(Vaga vaga) {
+        this.vagaId = vaga.getId();
+    }
+    public OffsetDateTime getInicio() {
+        return inicio;
+    }    
+    public void setInicio(OffsetDateTime inicio) {
+        this.inicio = inicio;
+    }
+    public OffsetDateTime getFim() {
+        return fim;
+    }
+    public void setFim(OffsetDateTime fim) {
+        this.fim = fim;
+    }
+    public Integer getTamanhoVeiculo() {
+        return tamanhoVeiculo;
+    }
+    public void setTamanhoVeiculo(Integer tamanhoVeiculo) {
+        this.tamanhoVeiculo = tamanhoVeiculo;
+    }
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+}
