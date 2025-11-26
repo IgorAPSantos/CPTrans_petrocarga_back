9138fb7591079a72a18fd1047b8ef02f69c1af6d
IgorAPSantos <igorppandre@hotmail.com>
2025-11-26 19:47:03 -0300

    refactor: getAll reservas by vagaId feat: get reservas by Agente

--- petrocarga/src/main/java/com/cptrans/petrocarga/controllers/ReservaRapidaController.java
+++ petrocarga/src/main/java/com/cptrans/petrocarga/controllers/ReservaRapidaController.java
@@ -1,9 +1,14 @@
 package com.cptrans.petrocarga.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cptrans.petrocarga.dto.ReservaRapidaRequestDTO;
 import com.cptrans.petrocarga.dto.ReservaRapidaResponseDTO;
+import com.cptrans.petrocarga.models.Agente;
 import com.cptrans.petrocarga.models.ReservaRapida;
 import com.cptrans.petrocarga.models.Vaga;
+import com.cptrans.petrocarga.services.AgenteService;
 import com.cptrans.petrocarga.services.ReservaRapidaService;
 import com.cptrans.petrocarga.services.VagaService;
+
+
 
 
 @RestController
@@ -25,6 +34,8 @@ public class ReservaRapidaController {
     private ReservaRapidaService reservaRapidaService;
     @Autowired
     private VagaService vagaService;
+    @Autowired
+    private AgenteService agenteService;
 
     @PreAuthorize("hasAnyRole('ADMIN', 'AGENTE')")
     @PostMapping()
@@ -34,5 +45,22 @@ public class ReservaRapidaController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(novaReservaRapida.toResponse());
     }
+    
+    @PreAuthorize("hasAnyRole('ADMIN', 'GESTOR') or #agenteId == authentication.principal.id")
+    @GetMapping("/{agenteId}")
+    public ResponseEntity<List<ReservaRapidaResponseDTO>> getReservasRapidas(@PathVariable UUID agenteId) {
+        Agente agente = agenteService.findById(agenteId);
+        List<ReservaRapidaResponseDTO> reservasRapidas = reservaRapidaService.findByAgente(agente).stream()
+                .map(ReservaRapida::toResponse)
+                .toList();
+        return ResponseEntity.ok(reservasRapidas);
+
+    }
+    
+    // @PreAuthorize("hasAnyRole('ADMIN','GESTOR', 'AGENTE')")
+    // @GetMapping()
+    // public ResponseEntity<List<ReservaRapidaResponseDTO>> getReservasRapidas(@RequestParam (required = false) UUID vagaId, @RequestParam (required = false) String placaVeiculo) {
+    //     return new String();
+    // }
     
 }
--- petrocarga/src/main/java/com/cptrans/petrocarga/repositories/ReservaRapidaRepository.java
+++ petrocarga/src/main/java/com/cptrans/petrocarga/repositories/ReservaRapidaRepository.java
@@ -6,11 +6,14 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cptrans.petrocarga.enums.StatusReservaEnum;
+import com.cptrans.petrocarga.models.Agente;
 import com.cptrans.petrocarga.models.ReservaRapida;
 import com.cptrans.petrocarga.models.Vaga;
 
 public interface ReservaRapidaRepository extends JpaRepository<ReservaRapida, UUID> {
+    public List<ReservaRapida> findByVaga(Vaga vaga);
     public List<ReservaRapida> findByVagaAndStatus(Vaga vaga, StatusReservaEnum status);
     public List<ReservaRapida> findByPlacaIgnoringCaseAndStatus(String placa, StatusReservaEnum status);
+    public List<ReservaRapida> findByAgente(Agente agente);
     public Integer countByPlaca(String placa);
 }
